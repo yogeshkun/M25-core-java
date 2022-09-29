19cf9f50468a785b6ef3fa6037324d30f77c2734
Dell <Dell@DESKTOP-GT2J9S9>
2022-09-29 10:23:09 +0530

    First Day2 commit

---
+++ Day2_IntroductionToJava/src/datatype/datatypeinjava.java
@@ -0,0 +1,15 @@
+package datatype;
+
+public class datatypeinjava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		String b = "Hello";
+		double c = 39.6;
+		long d=128;
+		System.out.println(a+b+c+d);
+		
+	}
+
+}
